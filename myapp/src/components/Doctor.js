2dead96ee632edbe1e66b04e06a32abc2121668a
Ali Abunada <31050806+Aliabunada@users.noreply.github.com>
2020-02-16 19:03:06 +0200

    Update Doctor.js

--- myapp/src/components/Doctor.js
+++ myapp/src/components/Doctor.js
@@ -1,110 +1,83 @@
 import React from 'react';
-import {Inject,ScheduleComponent , Day ,Week , WorkWeek , Month,Agenda,ViewDirective, ViewsDirective } from '@syncfusion/ej2-react-schedule'
-
+import { Inject, ScheduleComponent, Day, Week, WorkWeek, Month, Agenda, ViewDirective, ViewsDirective } from '@syncfusion/ej2-react-schedule'
 import Button from '@material-ui/core/Button';
-
 import Typography from '@material-ui/core/Typography';
-import  { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import axios from 'axios';
-
 export default function Doctor() {
-    const [datas, setDatas] = useState([]);
-    const [appoints , setAppoints]= useState([]);
-    console.log(localStorage.getItem('em'),"success send!!!");
-    var emailuser = localStorage.getItem('em')
-    // localStorage.clear();
-useEffect(() => {
-      axios.get('/getinfodoc/'+emailuser) 
-            .then(({ data }) =>{
-            setDatas(data);
-          })  
-          
-       
-          
-      },[]);
-     
-        console.log('data',datas._id);
+  const [datas, setDatas] = useState([]);
+  const [appoints, setAppoints] = useState([]);
+  var emailuser = localStorage.getItem('em')
+  useEffect(() => {
+    axios.get('/getinfodoc/' + emailuser)
+      .then(({ data }) => {
+        setDatas(data);
+      })
 
-function Appoinment(e) {
-  e.preventDefault();
+  }, []);
+  function Appoinment(e) {
+    e.preventDefault();
+    axios.get('/appoinmentfordoctor/' + datas._id)
+      .then(({ data }) => {
+        setAppoints(data)
 
-axios.get('/appoinmentfordoctor/'+ datas._id)
-.then(({ data }) =>{
-  setAppoints(data)
-  console.log(data)
-   
-   console.log('Appointment',appoints);
+      })
 
 
-})
-
-
-}
-    return (
+  }
+  return (
     <div>
- 
-
-  
-    <Typography component="h2" variant="h5">
+      <Typography component="h2" variant="h5">
         Name :  {datas.firstname}       {datas.lastname}
-        </Typography>
-<br></br>
-<Typography component="h4" variant="h5">
-        specialization :  {datas.specialize} 
-        </Typography>
-        <br></br>
-<Typography component="h4" variant="h5">
-        Mobilenumbew :  {datas.mobile} 
-        </Typography>
-       
-    
-       
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <Button
-            type="submit"
-            
-            variant="contained"
-            color="primary"
-           
-            onClick={Appoinment}
-          >
-          Table for Appoinments
-          </Button>
-          <br></br>
-        <br></br>
-        <br></br> <br></br>
-        <br></br>
-        <br></br>
-       
-        <div>
-        {appoints.length?<ScheduleComponent currentView='Month' width='100%' height='550px' selectedDate={new Date()} eventSettings={{  dataSource: appoints,}} readonly={true} startHour='8:00' endHour='15:00'>
-           <ViewsDirective > 
-  <ViewDirective option = 'month' ></ViewDirective>
-  <ViewDirective option = 'week' ></ViewDirective>
- <ViewDirective option = 'Day' startHour='8:00' endHour='15:00' ></ViewDirective>
- <ViewDirective option = 'TimelineDay'></ViewDirective>
- <ViewDirective option = 'TimelineMonth'></ViewDirective>
- </ViewsDirective>
-      <Inject services={[Day, Week, WorkWeek, Month, Agenda]}/>
+      </Typography>
       <br></br>
-    
-    
-      
-    </ScheduleComponent>
-        :<div>
-       <h1>click to see appoinments</h1>
-      </div>}
-      
-       
+      <Typography component="h4" variant="h5">
+        specialization :  {datas.specialize}
+      </Typography>
+      <br></br>
+      <Typography component="h4" variant="h5">
+        Mobilenumbew :  {datas.mobile}
+      </Typography>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <Button
+        type="submit"
 
+        variant="contained"
+        color="primary"
+
+        onClick={Appoinment}
+      >
+        Table for Appoinments
+          </Button>
+      <br></br>
+      <br></br>
+      <br></br> <br></br>
+      <br></br>
+      <br></br>
+
+      <div>
+        {appoints.length ? <ScheduleComponent currentView='Month' width='100%' height='550px' selectedDate={new Date()} eventSettings={{ dataSource: appoints, }} readonly={true} startHour='8:00' endHour='15:00'>
+          <ViewsDirective >
+            <ViewDirective option='month' ></ViewDirective>
+            <ViewDirective option='week' ></ViewDirective>
+            <ViewDirective option='Day' startHour='8:00' endHour='15:00' ></ViewDirective>
+            <ViewDirective option='TimelineDay'></ViewDirective>
+            <ViewDirective option='TimelineMonth'></ViewDirective>
+          </ViewsDirective>
+          <Inject services={[Day, Week, WorkWeek, Month, Agenda]} />
+          <br></br>
+
+
+
+        </ScheduleComponent>
+          : <div>
+            <h1>click to see appoinments</h1>
+          </div>}
+      </div>
     </div>
-  
-    </div>
 
-
-
-    );
+  );
 }
