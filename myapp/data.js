afaf42fa9570920f8a038c699352a8de21c731a0
Ali Abunada <31050806+Aliabunada@users.noreply.github.com>
2020-02-16 19:18:13 +0200

    Update data.js

--- myapp/data.js
+++ myapp/data.js
@@ -1,32 +1,44 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const URI = "mongodb+srv://Ali:ali@webteb-8zitg.mongodb.net/Webteb?retryWrites=true&w=majority"
-mongoose.connect(URI, {useNewUrlParser: true, useUnifiedTopology: true}); //, useMongoClient: true
+mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true }); //, useMongoClient: true
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log("We're Connected to database!");
+db.once('open', function () {
+      console.log("We're Connected to database!");
 });
 
-///____________________________ patient ______________________
+///____________________________ patient schema ______________________
 
 const patientschema = Schema({ //patient description schema
-      id:{type:Number , unique:true},
-  firstname:{type: String,
-        required: true},
-  lastname:{type: String,
-        required: true}, 
-  password:{type:String,
-        required: true},
-  email:{type:String,
-        required: true, unique: true,
-        lowercase: true},
-  mobile:{type:String,
-        required: true},
-  gender: {type: String,
-  },
-  address: {type: String,
-   }
+      id: { type: Number, unique: true },
+      firstname: {
+            type: String,
+            required: true
+      },
+      lastname: {
+            type: String,
+            required: true
+      },
+      password: {
+            type: String,
+            required: true
+      },
+      email: {
+            type: String,
+            required: true, unique: true,
+            lowercase: true
+      },
+      mobile: {
+            type: String,
+            required: true
+      },
+      gender: {
+            type: String,
+      },
+      address: {
+            type: String,
+      }
 
 })
 
@@ -35,80 +47,65 @@ const Userpatient = mongoose.model('Userpatient', patientschema);
 
 const Appoinments = Schema({
 
-      EndTime : {type: String},
-      Guid :  {type: String},
-      Id: {type: String},
-      Index : {type: String},
-      IsReadonly : {type: String},
-      StartTime : {type: String},
-      Subject : {type: String},
-      Patientemai:{type: String},
-      Doctorid :{type:String}
-    
+      EndTime: { type: String },
+      Guid: { type: String },
+      Id: { type: String },
+      Index: { type: String },
+      IsReadonly: { type: String },
+      StartTime: { type: String },
+      Subject: { type: String },
+      Patientemai: { type: String },
+      Doctorid: { type: String }
+
 })
 const Appoinmentsmodel = mongoose.model('Appoinmentsmodel', Appoinments);
 
-// const Specialization = Schema({
-//       id:{type:Number , unique:true},
-//       Doctor :{type: Schema.Types.ObjectId, ref: 'Userdoctor'},
-//       specialize: {type: String}
 
-// })
-// const Specializationmodel = mongoose.model('Specializationmodel', Specialization);
-
-// const Workingdays = Schema({
-//        id:{type:Number , unique:true},
-//       Doctor :{type: Schema.Types.ObjectId, ref: 'Userdoctor'},
-//       Workingdays: {type: String}
-// })
-// const Workingdaysmodel = mongoose.model('Workingdaysmodel', Workingdays);
-
-// const Auth = Schema({ //schema for the authentication 
-//       username: {type: String},
-//       email: {type: String},
-//       password: {type: String},
-//        token: {}
-//     })
-    
-//     const User = mongoose.model('User', Auth);
-    
-
-
-
-///____________________________Doctor ______________________
+///____________________________Doctor schema______________________
 
 
 
 
 const doctorschema = Schema({ //Doctor description schema
-      
-      firstname:{type: String,
-            required: true},
-      lastname:{type: String,
-            required: true}, 
-      password:{type:String,
-            required: true},
-      email:{type:String,
-            required: true,
-            unique:true
-            },
-      mobile:{type:String,
-            required: true},
-      gender: {type: String,
+
+      firstname: {
+            type: String,
+            required: true
       },
-      address: {type: String,
-       },
-    img :{type: String},
-    Workingdays: {type: String},
-    specialize: {type: String},
-    smallbrief:{type: String}
-  })
-  
-  const Userdoctor = mongoose.model('Userdoctor', doctorschema);
-  
-  
+      lastname: {
+            type: String,
+            required: true
+      },
+      password: {
+            type: String,
+            required: true
+      },
+      email: {
+            type: String,
+            required: true,
+            unique: true
+      },
+      mobile: {
+            type: String,
+            required: true
+      },
+      gender: {
+            type: String,
+      },
+      address: {
+            type: String,
+      },
+      img: { type: String },
+      Workingdays: { type: String },
+      specialize: { type: String },
+      smallbrief: { type: String }
+})
+
+const Userdoctor = mongoose.model('Userdoctor', doctorschema);
+
+
 
 /////--------------------------------//----------------------------------------
 
 
-module.exports={Userdoctor, Userpatient,Appoinmentsmodel};
+module.exports = { Userdoctor, Userpatient, Appoinmentsmodel };
